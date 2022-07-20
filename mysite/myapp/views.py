00b7dca33efb4a12312d71df79afea2198b4910f
Islompulatov <98391405+Islompulatov@users.noreply.github.com>
2022-07-20 13:28:59 +0200

    django

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 # Create your views here.
 
+
+
 def index(response, id):
 	ls = ToDoList.objects.get(id = id)
-	return render(response, "myapp/base.html", {})
+
+	return render(response, "myapp/list.html", {"ls":ls})
 
 	
 
