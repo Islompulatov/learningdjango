67da0aa9bc0a06c5486155d2d04081da9c717311
Islompulatov <98391405+Islompulatov@users.noreply.github.com>
2022-08-01 14:47:03 +0200

    created simple froms

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 
 
@@ -10,7 +11,18 @@ def index(response, id):
 
 	return render(response, "myapp/list.html", {"ls":ls})
 
-	
-
-def home(response):
+def home(respocd nse):
 	return render(response, "myapp/home.html", {})
+
+def create(response):
+	if response.method == "POST":
+		form = CreateNewList(response.POST)
+		if form.is_valid():
+			n = form.cleaned_data["name"]
+			t = ToDoList(name = n)
+			t.save()
+		return HttpResponseRedirect("/%i" %t.id) 	
+
+	else:
+		form = CreateNewList()
+	return render(response, "myapp/create.html", {"form":form})	
