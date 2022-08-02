4e3e03af36da0aebed2ddc53852905b0cc878b08
Islompulatov <98391405+Islompulatov@users.noreply.github.com>
2022-08-02 15:02:06 +0200

    custom form created

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -8,10 +8,28 @@ from .forms import CreateNewList
 
 def index(response, id):
 	ls = ToDoList.objects.get(id = id)
+	if response.method == "POST":
+		print(response.POST)
+		if response.POST.get("save"):
+			for item in ls.item_set.all():
+				if response.POST.get("c" + str(item.id)) == "clicked":
+					item.complete == True
+				else:
+					item.complete == False	
+				item.save()	
+
+		elif response.POST.get("newItem"):
+			txt = response.POST.get("new")	
+
+			if len(txt)>2: 
+				ls.item_set.create(text = txt, complete=False)
+			else:
+				print("invalid item")	
+
 
 	return render(response, "myapp/list.html", {"ls":ls})
 
-def home(respocd nse):
+def home(response):
 	return render(response, "myapp/home.html", {})
 
 def create(response):
